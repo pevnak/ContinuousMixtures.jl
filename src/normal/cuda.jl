354fc81367c673fb03a81d0a31feeeb07b8f2549
pevnak <pevnak@protonmail.ch>
2025-05-31 22:06:00 +0200

    CUDA for normal seems to work, except that it does not support gradient with respect to x, as it would likely require one more kernel and I am lazy now

--- src/normal/cuda.jl
+++ src/normal/cuda.jl
@@ -29,7 +29,7 @@ function logprob(m::CuGMM{T}, x::CuMatrix{T}) where {T}
     n_dimension == size(x, 1) || error("dimension does not match")
 
     logp = similar(μ, n_components, n_observations)
-    max_ = similar(logits, n_observations)
+    max_ = similar(μ, n_observations)
 
     @cuda threads = n_observations blocks = n_components logprob_normal_kernel!(logp, max_, μ, Σ, x, n_components, n_observations, n_dimension)
 
@@ -47,7 +47,7 @@ end
 	Moreover, since the `n_dims` can be larger than number of threads, the thread has to operate over small range
 	denoted as Δ.
 """
-function ∇logprob_normal_kernel!(∇μ, ∇Σ, ∇x, ∇logp, μ, Σ, x, n_components, n_observations, n_dimension, Δ)
+function ∇logprob_normal_kernel!(∇μ, ∇Σ, ∇logp, μ, Σ, x, n_components, n_observations, n_dimension, Δ)
     i = blockIdx().x # index of the component
     k = threadIdx().x # index of the dimension of the logit
 
@@ -56,15 +56,17 @@ function ∇logprob_normal_kernel!(∇μ, ∇Σ, ∇x, ∇logp, μ, Σ, x, n_com
 
     range_k = ((k-1)*Δ+1):(k*Δ)
     # now we increment the gradient
-    for kᵢ in range_k
+    @inbounds for kᵢ in range_k
         kᵢ > n_dimension && continue
-        ∇μᵢ = 0
+        ∇μᵢ = 0f0
+        ∇Σᵢ = 0f0
         for j in 1:n_observations
             ∇μᵢ -= (μ[kᵢ,i] - x[kᵢ, j])*∇logp[i,j] / Σ[kᵢ,i]
+            ∇Σᵢ += ((μ[kᵢ,i] - x[kᵢ, j])^2/(2*Σ[kᵢ,i]^2) - 1/(2*Σ[kᵢ,i])) * ∇logp[i,j]
             # ∇x[k,j] += (μ[k,i] - x[k, j])*∇logp[i,j]/Σ[k,i]
-            # ∇Σ[k,i] += ((μ[k,i] - x[k, j])^2/(2*Σ[k,i]^2) - 1/(2*Σ[k,i])) * ∇logp[i,j]
         end
-        ∇μ[k,i] = ∇μᵢ
+        ∇μ[kᵢ,i] = ∇μᵢ
+        ∇Σ[kᵢ,i] = ∇Σᵢ
     end
     return (nothing)
 end
@@ -75,20 +77,19 @@ end
 
 	the gradient of the `logprob` with respect to the logits
 """
-function ∇logprob(∇logprobs::CuMatrix{T}, m::CuGMM{T}, x::CuArray{<:Integer,2}) where {T<:Real}
+function ∇logprob(∇logp::CuMatrix{T}, m::CuGMM{T}, x::CuMatrix{T}) where {T<:Real}
     μ, Σ = m.μ, m.Σ
-    n_components = size(logits, 3)
+    n_components = size(μ, 2)
     n_observations = size(x, 2)
-    n_dimension = size(logits, 2)
-    n_categories = size(logits, 1)
+    n_dimension = size(μ, 1)
+    n_dimension == size(x, 1) || error("dimension does not match")
     size(∇logprobs, 2) == n_observations || error("dimension does not match")
-    n_dimension == size(x, 1) || error("n_dimensions does not match")
     Δ = cld(n_dimension, 1024)
 
-    ∇μ, ∇Σ, ∇x = similar(μ), similar(Σ), similar(x)
-    @cuda threads = cld(n_dimension, Δ) blocks = n_components ∇logprob_normal_kernel!(∇μ, ∇Σ, ∇x, ∇logp, μ, Σ, x, n_components, n_observations, n_dimension, Δ)
+    ∇μ, ∇Σ = similar(μ), similar(Σ)
+    @cuda threads = cld(n_dimension, Δ) blocks = n_components ∇logprob_normal_kernel!(∇μ, ∇Σ, ∇logp, μ, Σ, x, n_components, n_observations, n_dimension, Δ)
 
-    return (Tangent{GM}(μ = ∇μ, Σ = ∇Σ), NoTangent())
+    return (Tangent{CuGMM{T}}(μ = ∇μ, Σ = ∇Σ), NoTangent())
 end
 
 """
@@ -101,12 +102,10 @@ end
 	Moreover, since the `n_dims` can be larger than number of threads, the thread has to operate over small range
 	denoted as Δ.
 """
-function ∇logprob_fused_kernel!(∇μ, ∇Σ, ∇x, ∇y, μ, Σ, x, logp, mx, sumexp, n_components, n_observations, n_dimension, ::Val{max_threads}) where {max_threads}
+function ∇logprob_fused_kernel!(∇μ, ∇Σ, ∇y, μ, Σ, x, logp, mx, sumexp, n_components, n_observations, n_dimension, ::Val{max_threads}) where {max_threads}
     i = blockIdx().x # index of the component
     k = threadIdx().x # index of the dimension of the logit
     Δ = cld(n_dimension, max_threads)
-    local_storage = CuStaticSharedArray(Float32, max_threads * n_categories)
-    offset = (k - 1) * n_categories
 
     # check bounds such that we do not have to care about number of allocated threads
     i > n_components && return (nothing)
@@ -114,34 +113,31 @@ function ∇logprob_fused_kernel!(∇μ, ∇Σ, ∇x, ∇y, μ, Σ, x, logp, mx,
     range_k = ((k-1)*Δ+1):(k*Δ)
     for kᵢ in range_k
         kᵢ > n_dimension && continue
-        for o in 1:n_categories
-            local_storage[offset+o] = 0
-        end
-        @inbounds for j in 1:n_observations
+        ∇μᵢ = 0f0
+        ∇Σᵢ = 0f0
+        for j in 1:n_observations
             o = ∇y * exp(logp[i, j] - mx[j]) / sumexp[j]
-            xⱼ = x[kᵢ, j]
-            local_storage[offset+xⱼ] += o
+            ∇μᵢ -= (μ[kᵢ,i] - x[kᵢ, j])*o / Σ[kᵢ,i]
+            ∇Σᵢ += ((μ[kᵢ,i] - x[kᵢ, j])^2/(2*Σ[kᵢ,i]^2) - 1/(2*Σ[kᵢ,i])) * o
         end
-
-        for o in 1:n_categories
-            ∇logits[o, kᵢ, i] = local_storage[offset+o]
-        end
+        ∇μ[kᵢ,i] = ∇μᵢ
+        ∇Σ[kᵢ,i] = ∇Σᵢ
     end
     return (nothing)
 end
 
-function ∇logprob_fused(∇y, m::GM, x::CuArray{<:Integer,2}, mx, logp, sumexp)  where {T<:Real, GM<:GaussianMixture{<:CuArray{T,3}}}
+function ∇logprob_fused(∇y, m::CuGMM{T}, x::CuMatrix{T}, mx, logp, sumexp)  where {T<:Real}
     μ, Σ = m.μ, m.Σ
     max_threads = 64
-    n_components = size(logits, 3)
+    n_components = size(μ, 2)
     n_observations = size(x, 2)
-    n_dimension = size(logits, 2)
-    n_categories = size(logits, 1)
-    n_dimension == size(x, 1) || error("n_dimensions does not match")
+    n_dimension = size(μ, 1)
+    n_dimension == size(x, 1) || error("dimension does not match")
+    size(∇logprobs, 2) == n_observations || error("dimension does not match")
     Δ = cld(n_dimension, max_threads)
-    ∇μ, ∇Σ, ∇x = similar(μ), similar(Σ), similar(x)
+    ∇μ, ∇Σ = similar(μ), similar(Σ)
 
-    @cuda threads = cld(n_dimension, Δ) blocks = n_components ∇logprob_fused_kernel!(∇μ, ∇Σ, ∇x, ∇y, μ, Σ, x, logp, mx, sumexp, n_components, n_observations, n_dimension, Val(n_categories), Val(max_threads))
+    @cuda threads = cld(n_dimension, Δ) blocks = n_components ∇logprob_fused_kernel!(∇μ, ∇Σ, ∇y, μ, Σ, x, logp, mx, sumexp, n_components, n_observations, n_dimension, Val(max_threads))
 
-    return (Tangent{GM}(μ = ∇μ, Σ = ∇Σ), NoTangent())
+    return (Tangent{CuGMM{T}}(μ = ∇μ, Σ = ∇Σ), NoTangent())
 end
